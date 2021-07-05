00b0e0c6e491a2e68efa5a368cbd9b0364b35914
Yudai Nakajima <kituneudon97@gmail.com>
2021-07-05 15:36:10 +0900

    #10 店舗選択フォームのstoryをバックエンドに依存せずに動作するよう変更

--- src/components/Organisms/StoreNameForm/StoreNameForm.stories.tsx
+++ src/components/Organisms/StoreNameForm/StoreNameForm.stories.tsx
@@ -1,14 +1,43 @@
-// import { FC, useState } from 'react';
+import { FC, useState } from 'react';
+import MockAdapter from 'axios-mock-adapter';
+import axios from 'axios';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import Index from '.';
+import { employeeType } from '../../../types/Employee';
+import responseData from './MockApiServerResponseData';
 
 export default {
   component: Index,
   title: 'molecules/StoreNameForm',
+  decorators: [
+    (story: () => JSX.Element): JSX.Element => <Router>{story()}</Router>,
+  ],
 };
 
-// export const Default: FC = () => {
-//   // eslint-disable-next-line
-//   const [selectedStoreId, setSelectedStoreId] = useState(1);
+const defaultEmployee = { id: NaN, first_name: '', last_name: '' };
 
-//   return <Index setSelectedStoreId={setSelectedStoreId} />;
-// };
+const mock = new MockAdapter(axios);
+
+export const Default: FC = () => {
+  // eslint-disable-next-line
+  const [selectedStoreId, setSelectedStoreId] = useState(1);
+  // eslint-disable-next-line
+  const [selectedEmployeeId, setSelectedEmployeeId] = useState(1);
+  // eslint-disable-next-line
+  const [selectedEmployee, setSelectedEmployee] = useState<employeeType | null>(
+    defaultEmployee,
+  );
+
+  mock
+    .onGet('http://localhost:3000/api/v1/stores?count=50')
+    .reply(200, responseData);
+
+  return (
+    <Index
+      setSelectedStoreId={setSelectedStoreId}
+      setSelectedEmployeeId={setSelectedEmployeeId}
+      setSelectedEmployee={setSelectedEmployee}
+    />
+  );
+};
